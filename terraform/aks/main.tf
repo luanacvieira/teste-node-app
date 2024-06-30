f6030095fff4c62f75ed32fb397b0fe6a9aca996
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 01:12:07 -0300

    terraform

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_L8as_v3"
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
