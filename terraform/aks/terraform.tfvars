9b3ef269e12b75d3ad97fab21a500922fdf053d6
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 02:04:51 -0300

    voltando versao

---
+++ terraform/aks/terraform.tfvars
@@ -0,0 +1,4 @@
+client_id         = "ea0f02da-f58e-490d-89d1-7fb4dbef53a7"
+client_secret     = "QT98Q~XSoVgBhhRq7-X5_eZeF2N.IaxtmXwn~anY"
+subscription_id   = "8c35b025-ce1a-48a1-8491-b82ae7d9249e"
+resource_group_name = "myResourceGroup"
--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -1,3 +1,18 @@
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the AKS cluster"
   type        = string
